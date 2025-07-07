7edf5ae93a9dd0f916125235209e3046020413b3
Maxime RENAULT <maxime.renault13@gmail.com>
2025-07-07 15:12:59 +0200

    Fix import + Jenkinsfile

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 from io import StringIO
-from etl import extract_data, transform_data, load_data
+from app.etl import extract_data, transform_data, load_data
 
 # Test for data extraction
 def test_extract_data():
